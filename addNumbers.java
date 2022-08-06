2fc23b9614146f152d15c98e09d5c633b1f88f69
kotti durga sai pranith <saikotti2@gmail.com>
2022-08-06 14:51:26 +0530

    the code for sum of array

---
+++ addNumbers.java
@@ -0,0 +1,15 @@
+class AddNumbers
+{
+public static int arraySum(int[] a)
+{
+int sum=0,i;
+for(i=0;i<n;i++) sum+=i;
+return sum;
+}
+public static void main(String[] args)
+{
+ 
+int a[]={1,2,3,4,5};
+System.out.println("the sum of array is"+arraySum(a));
+}
+}
